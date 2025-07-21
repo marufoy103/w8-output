11605989b0f1cae2fa617772fddc35f1bdacfe94
marufoy103 <ron.qianhong@gmail.com>
2025-07-21 15:07:14 +0900

    追加：ログイン情報を保存、サインインすると自動で保存した情報が入力される機能

--- database/app.js
+++ database/app.js
@@ -1,6 +1,19 @@
-// フォームから入力されたログイン情報を取得する
-var username = document.getElementById('username').value;
-var password = document.getElementById('password').value;
-// ログイン情報をLocalStorageに保存する
-localStorage.setItem('username', username);
-localStorage.setItem('password', password);
+window.addEventListener('DOMContentLoaded', () => {
+    const savedEmail = localStorage.getItem('email');
+    const savedPassword = localStorage.getItem('password');
+    if(savedEmail) {
+        document.getElementById('email').value = savedEmail;
+    }
+    if(savedPassword) {
+        document.getElementById('password').value = savedPassword;
+    }
+})
+document.getElementById('login-form').addEventListener('submit', function (e) {
+    e.preventDefault(); // ページリロードを防ぐ
+
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    // localStorage に保存
+    localStorage.setItem('email', email);
+    localStorage.setItem('password', password);
+  });
